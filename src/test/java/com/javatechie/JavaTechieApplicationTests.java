2607588f69d802c5899f33c8076093d235226769
Manjunath  Reddy <manjunathreddy24101997@gmail.com>
2021-05-31 00:20:42 +0530

    first commit

---
+++ src/test/java/com/javatechie/JavaTechieApplicationTests.java
@@ -0,0 +1,20 @@
+package com.javatechie;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTechieApplicationTests {
+
+	Logger logger = LoggerFactory.getLogger(JavaTechieApplicationTests.class);
+	@Test
+	public void contextLoads() {
+		logger.info("Test Case Executing...");
+		assertEquals(true,true);
+	}
+
+}
